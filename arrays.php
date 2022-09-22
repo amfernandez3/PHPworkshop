3cd19934a48c5fccdda6b51d08c622ee146a2847
DAWDU207 <DAWDU207@acarballeira.local>
2022-09-22 17:35:55 +0200

    commit

---
+++ arrays.php
@@ -0,0 +1,14 @@
+<?php
+$mi_array = [0=>"Hola",10=>"adios"];
+
+foreach ($mi_array as &$valor) {
+    echo($valor);
+}
+
+
+//función para recorrer el array:
+print_r($mi_array);
+
+//Añadir elementos
+$mi_array["A"] = "mensaje";
+?>
