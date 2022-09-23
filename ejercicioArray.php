5758e3d56b9a4a6bdbba680ee06e346b6e7c6308
alejandromef <alejandro_meno@hotmail.com>
2022-09-23 22:47:49 +0200

    Added array info

--- ejercicioArray.php
+++ ejercicioArray.php
@@ -7,12 +7,14 @@ function recorrerArray($arr) {
 
     //print_r($arr);
     foreach($arr as $value) {
-        print($value);
         if(is_array($value)){
             
             foreach($value as $value2){
                 print($value);
             }
+        }
+        else{
+            print($value);
         }
       } 
 }
--- lab.php
+++ lab.php
@@ -3,7 +3,18 @@
 Trabajo con permisos y desplazamiento de bits
 */
 
-$numero  = 5;
+$numeros  = [2,3,4,5,6];
+$numeros2 = array(2 => "2", 3 => "tres");
+$arrayClaveValor = ["nombre"=>"Alejandro", "apellido"=>"Meno"];
+print_r($numeros);
+
+/*foreach($numeros as $var){
+    echo ("Array: ". $var . "\n");
+}
 
 
+*/
+var_dump($numeros);
+$arrayPrueba = [1,3, "color" => "azul", "perro", 2 => 7];
+var_dump($arrayPrueba);
 ?>
